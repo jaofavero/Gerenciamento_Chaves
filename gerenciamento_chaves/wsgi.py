5622b42168aed1e8dd31f828ba5a948b48d1e607
Joao Favero <jvfavero@gmail.com>
2025-10-18 20:36:56 -0300

    Revert "mudança do nome do projeto"
    This reverts commit 949d8afa774b01acc11256cbe6122c8d63d69f53.

--- gerenciamento_chaves/wsgi.py
+++ gerenciamento_chaves/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetogerenciamentoativos project.
+WSGI config for gerenciamento_chaves project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetogerenciamentoativos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciamento_chaves.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetogerenciamentoativos.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciamento_chaves.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
