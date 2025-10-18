949d8afa774b01acc11256cbe6122c8d63d69f53
Joao Favero <jvfavero@gmail.com>
2025-10-18 20:23:58 -0300

    mudança do nome do projeto

--- projetogerenciamentoativos/wsgi.py
+++ projetogerenciamentoativos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gerenciamento_chaves project.
+WSGI config for projetogerenciamentoativos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciamento_chaves.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetogerenciamentoativos.settings')
 
 application = get_wsgi_application()
