58f307cdc8b7abb59b3209c611d9ccdf48962a43
Joao Favero <jvfavero@gmail.com>
2025-10-22 05:48:30 -0300

    att area admin

--- principal/admin.py
+++ principal/admin.py
@@ -13,13 +13,21 @@ class UsuarioAdmin(UserAdmin):
     # Adiciona os campos 'cpf' e 'contato' ao 'add_fieldsets'
     # (os campos exibidos ao CRIAR um novo usuário).
     add_fieldsets = UserAdmin.add_fieldsets + (
+        # Adiciona Nome, Sobrenome e Email à primeira seção padrão
+        (None, {'fields': ('first_name', 'last_name', 'email')}), 
+        # Adiciona a seção de Informações Customizadas
         ('Informações Customizadas', {'fields': ('cpf', 'contato')}),
+        # Adiciona Grupos à seção de Permissões padrão
+        ('Permissões', {'fields': ('groups',)}), 
     )
+            
+
 
     list_display = UserAdmin.list_display + ('contato', 'cpf')
 
 class ChaveAdmin(admin.ModelAdmin):
     list_display = ('nome', 'status', 'portador_atual', 'grupo_permissao', 'excluido')
+
 
 class HistoricoEmprestimoAdmin(admin.ModelAdmin):
     list_display = ('chave', 'usuario', 'data_hora', 'acao')
