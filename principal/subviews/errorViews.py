57888e28dac2f4cad3055a200468181d5d77e31e
Joao Favero <jvfavero@gmail.com>
2025-11-02 14:08:17 -0300

    Corrigido pagina de erro 404.html customizada

--- principal/subviews/errorViews.py
+++
@@ -1,9 +0,0 @@
-# Author: João Victor Marques Favero
-from django.shortcuts import render
-
-def custom_page_not_found_view(request, exception):
-    """
-    View customizada para renderizar a página 404.
-    """
-    # Renderiza o template de erro 404
-    return render(request, 'error/404.html', status=404)
